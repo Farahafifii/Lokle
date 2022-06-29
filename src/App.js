aa79c6788dbf0cb6adcc484891d17068eda59c89
Farahafifii <farah-afifi@hotmail.com>
2022-06-29 02:30:27 +0200

    login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import Login from './login';
-
+import Home from './Home';
+import {BrowserRouter} from 'react-router-dom';
+import {
+  BrowserRouter as 
+  Router,
+  Switch,
+  Route,
+  Redirect,
+  Routes
+} from "react-router-dom";
+  
 function App() {
   return (
     <div className="App">
       <div className="content">
-        <Login/>
+      <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Login />}>
+           </Route>
+      <Route path="/Home" element={<Home />}> </Route>
+         </Routes>
+       </BrowserRouter>
+
       </div>
     </div>
   );
--- src/login.js
+++ src/login.js
@@ -1,16 +1,23 @@
+import { Link } from "react-router-dom";
 const login  = () => {
     return ( 
         <div className="login">
-            <p0 href skip />
+             <nav><Link to="/Home" className="text-Link">Skip{' >'}  </Link></nav>
+             <br/>
             <h1>Lokle</h1>
             <p>Mobile Number</p>
             <input className="e-input" type="text" placeholder="Enter mobile Number" />
             <p>Password</p>
             <input className="e-input" type="password" placeholder="Enter password" />
-            <label1><input type="checkbox"/>href Remember Me</label1>
+            <br/>
+            <label1><input type="checkbox"/> Remember Me<nav><Link to="/forgot" className="text-LinkForgot">Forgot Password?</Link></nav></label1>
+            <br/>
+            <br/>
+            <nav><Link to="/Home"><button className="button_Login">LOGIN</button> </Link></nav>
+            <br/>
+            <p2>Don't have an account?</p2><nav><Link to="/signup" className="text-LinkSign">Sign Up Here</Link></nav>
             </div>
             
      );
 }
- 
 export default login ;
