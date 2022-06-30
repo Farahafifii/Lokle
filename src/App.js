f330e2d28b6510f0300fe8c17efc76dc29ca4999
Farahafifii <farah-afifi@hotmail.com>
2022-06-30 04:27:57 +0200

    register

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Login from './login';
 import Home from './Home';
+import Register from './register';
 import {BrowserRouter} from 'react-router-dom';
 import {
   BrowserRouter as 
@@ -9,17 +10,16 @@ import {
   Route,
   Routes
 } from "react-router-dom";
-//
-  
+
 function App() {
   return (
     <div className="App">
       <div className="content">
       <BrowserRouter>
      <Routes>
-      <Route path="/login" element={<Login />}>
-           </Route>
+      <Route path="/login" element={<Login />}></Route>
       <Route path="/Home" element={<Home />}> </Route>
+      <Route path="/register" element={<Register />}></Route>
          </Routes>
        </BrowserRouter>
 
--- src/register.js
+++ src/register.js
@@ -1,7 +1,104 @@
-const register = () => {
-    return (  <div className="login">
-    <h2>Login</h2>
-    </div> );
+import { Link } from "react-router-dom";
+import React from "react";
+//import { useState,useEffect } from "react";
+
+export default class register extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            name:"",
+            phone:"",
+            password:"",
+            passConfirm:"",
+            email:"",
+            city:"",
+            nationality:"",
+            country :"",
+            gender:"",
+            birthdate:""
+        }
+    }
+    
+    submit(){
+        console.log(this.state);
+    }
+    render() {
+         return ( 
+            <div className="login">
+                <body>
+                <div class="social-container2">
+                      <ul class="social-icons2">
+                      <li><a href = {"#"}><i class="fa fa-user" style={{fontSize:"35px"}}></i></a></li>
+                      </ul></div>
+                <nav name="login">
+                <p>Name</p>
+                <input className="e-input" value={this.state.name} type="text" name="name" placeholder="Name..." onChange={(data)=>{this.setState({name:data.target.value})}} />
+                <p>Phone Number</p>
+                <input className="e-input" value={this.state.phone} type="text" name="phone" placeholder="Phone Number" onChange={(data)=>{this.setState({phone:data.target.value})}}/>
+                <br/>
+                <div  style={{display:"inline-block",float:"left"}}>
+                    <p style={{display:"block"}}>Email address</p>
+                    <input style={{width:"165px"}}className="e-input" value={this.state.email} type="text" name="email" placeholder="Email Address" onChange={(data)=>{this.setState({email:data.target.value})}}/>
+                </div>
+                <br/>
+                <div style={{display:"inline-block", float:"right",marginTop:"-22px"}}>
+                    <p style={{display:"block"}}>City</p>
+                    <select className="selectOption"  onChange={(data)=>{this.setState({city:data.target.value})}}> 
+                         <option value="">City...</option>
+                         <option value="CA">Cairo</option>
+                         <option value="ALX">Alexandria</option>
+                         <option value="ASW">Aswan</option>
+                         <option value="SSh">Sharm el Sheikh</option></select>
+                </div>
+                <br/>
+                <div style={{display:"inline-block", float:"left"}}>
+                    <p style={{display:"block"}}>Nationality</p>
+                    <select style={{width:"165px"}}className="selectOption" onChange={(data)=>{this.setState({nationality:data.target.value})}}> 
+                         <option value="">Nationality...</option>
+                         <option value="EGY">Egypt</option>
+                         <option value="ARE">United Arab Emirates</option>
+                         <option value="GBR">United Kingdom</option>
+                         <option value="USA">United States</option></select>
+                </div>
+                <div style={{display:"inline-block", float:"right",marginTop:"-25px"}}>
+                    <p style={{display:"block"}}>Country</p>
+                    <select className="selectOption" onChange={(data)=>{this.setState({country:data.target.value})}}> 
+                         <option value="">Country...</option>
+                         <option value="EGY">Egypt</option>
+                         <option value="ARE">United Arab Emirates</option>
+                         <option value="GBR">United Kingdom</option>
+                         <option value="USA">United States</option></select>
+                </div>
+                <br/>
+                <div  style={{display:"inline-block",float:"right",marginTop:"-25px"}}>
+                    <p style={{display:"block"}}>Confirm Password</p>
+                    <input style={{width:"165px"}}className="e-input" value={this.state.passwordConfirm} type="password" name="passwordConfirm" placeholder="Password..." onChange={(data)=>{this.setState({passConfirm:data.target.value})}}/>
+                </div>
+                <div  style={{display:"inline-block",float:"left",marginTop:"-25px"}}>
+                    <p style={{display:"block"}}>Password</p>
+                    <input style={{width:"165px"}}className="e-input" value={this.state.password} type="password" name="password" placeholder="Password..." onChange={(data)=>{this.setState({password:data.target.value})}}/>
+                </div>
+                <br/>
+                <div style={{display:"inline-block",float:"right",marginRight:"5px"}}>
+                    <p style={{display:"block"}}>BirthDate</p>
+                    <input style={{width:"165px"}} type="date" placeholder="Enter BirthDate"value={this.state.birthdate} onChange={(data)=>{this.setState({birthdate:data.target.value})}} name='birthdate'/>
+                </div>
+                <div style={{display:"inline-block", float:"right"}}>
+                    <p style={{display:"block"}}>Gender</p>
+                    <select className="selectOption" onChange={(data)=>{this.setState({gender:data.target.value})}}> 
+                         <option value="">Gender...</option>
+                         <option value="FEM">Female</option>
+                         <option value="MA">Male</option></select>
+                </div>
+                </nav>
+                <br/>
+                <br/>
+                <nav><Link to="/Home"><button onClick={()=>{this.submit()}} className="button_Login">SIGN UP</button> </Link></nav>
+                <br/>
+                </body>
+                </div>
+                
+         )
+    }
+   
 }
- 
-export default register;
